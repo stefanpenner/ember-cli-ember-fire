af073eb96dc40fb2efd350d085a2c80d1185f838
Stefan Penner <stefan.penner@gmail.com>
2014-05-15 08:37:29 -0400

    fix JSHINT

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,3 +1,3 @@
 export default DS.FirebaseAdapter.extend({
-  firebase: new Firebase('https://' + window.ENV.firebase_instance + '.firebaseio.com')
+  firebase: new window.Firebase('https://' + window.ENV.firebase_instance + '.firebaseio.com')
 });
