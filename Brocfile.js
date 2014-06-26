61679f97d379fdbed669917ddc26f36184dd77d4
Stefan Penner <stefan.penner@gmail.com>
2014-06-26 10:21:01 -0400

    some cleanup, emberfire  as an ember-addon is still not quite working though

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,4 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
-
 module.exports = app.toTree();
